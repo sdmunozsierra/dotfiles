d6e3d920e0cd52113c14283a7d548e58c66ea933
Sergio David Munoz <sdmunozsierra@miners.utep.edu>
2021-01-10 19:27:25 -0500

    Reworked stow-it.sh now a profile is required

--- scripts/stow-it.sh
+++ scripts/stow-it.sh
@@ -1,65 +1,104 @@
 #!/bin/bash
 # This script will install profile specific dotfiles with stow.
-# In case of not a profile a default configuration will be installed.
+# You must specify a profile. Use default for avoiding profile specific packages.
 # Use: $ ./stow-it.sh justin
 
-PROF=$1  # justin, edward, leidos
+# TODO add default or --none flag to install only default packages
+
+# Validate Input
+VALID_PROFILES=("justin" "edward" "leidos")
+if [[ ! " ${VALID_PROFILES[@]} " =~ " $1 " ]]; then
+    echo "Invalid profile $1"
+    echo "Please use any of the following profiles: ${VALID_PROFILES[@]}"
+    exit
+fi
+
+# Set DIR as home folder destination
+PROF=$1
 DIR="$HOME/dotfiles"
 cd "$DIR" || exit
 
+# Find packages per profile
 PROFILE_PKGS="$(find . -mindepth 2 -maxdepth 2  ! -name '.*' -type d | sed "s|./||")"
+PROFILE_PKGS=("${PROFILE_PKGS[@]/.git/}")     # Remove git from Profile pkgs
+
+# Find packages not in profiles
 DEFAULT_PKGS="$(find . -maxdepth 1  ! -name '.*' -type d | sed "s|./||")"
+DEFAULT_PKGS=("${DEFAULT_PKGS[@]/scripts/}")  # Remove scripts from Default pkgs
+DEFAULT_PKGS=("${DEFAULT_PKGS[@]/.git/}")     # Remove git from Default pkgs
 
-DEFAULT_PKGS=("${DEFAULT_PKGS[@]/scripts/}")  # Remove pkg from Default pkgs
-DEFAULT_PKGS=("${DEFAULT_PKGS[@]/.git/}")  # Remove pkg from Default pkgs
+package_cleaner(){
+    local profile_name=$1
+    local profile_pkgs=$2
+    local default_pkgs=$3
 
-echo "Setting $PROF profile specific packages:"
-echo "$PROFILE_PKGS"
-for PKG in ${PROFILE_PKGS[@]}; do
-    if [[ $PKG == */$PROF ]] ; then
-        PKG_NAME=${PKG%/*}
-        echo "Stow $PROF profile: $PKG_NAME"
-        cd $PKG_NAME
-        # Stow and solve conflicts if any
-        CONFLICTS=$(stow --no $PROF 2>&1 | awk '/\* existing target is/ {print $NF}')
-        if [ -z "$CONFLICTS" ]; then
-            stow --verbose $PROF -t ~
+    # Remove packages that do not correspond to the user profile
+    for prof_pkg in ${profile_pkgs[@]}; do
+        if [[ ! $prof_pkg == */$profile_name ]]; then
+            profile_pkgs=("${profile_pkgs[@]/$prof_pkg}")
+        # Remove default packages that corresponded to a profile
         else
-            # In case of a clonflict remove Local version and stow the Git version.
-            echo "Found the following conflicts:" 
-            for filename in ${CONFLICTS[@]}; do
-                echo $filename
-                if [[ -f $HOME/$filename || -L $HOME/$filename ]]; then
-                    echo "DELETE: $filename"
-                    rm -f "$HOME/$filename"
+            for def_pkg in ${default_pkgs[@]}; do
+                if [[ "$def_pkg" == "${prof_pkg%%/*}" ]]; then
+                    default_pkgs=("${default_pkgs[@]/$def_pkg}")
+                    break
                 fi
             done
-            stow --verbose $PROF -t ~
         fi
-        DEFAULT_PKGS=("${DEFAULT_PKGS[@]/$PKG_NAME}")  # Remove pkg from Default pkgs
-        cd $DIR  # Go back to root
-    fi
-done
+    done
+    CLEANED_PKGS=(${profile_pkgs[@]} ${default_pkgs[@]})
+    echo "Stowing the following packages: ${CLEANED_PKGS[@]}"
+}
 
-echo "Setting default packages:"
-echo "$DEFAULT_PKGS"
-for PKG in ${DEFAULT_PKGS[@]}; do
-    cd $DIR  # Go back to root
-    echo "Stow default profile: $PKG"
-    CONFLICTS=$(stow --no $PKG 2>&1 | awk '/\* existing target is/ {print $NF}')
-    if [ -z "$CONFLICTS" ]; then
-        stow --verbose $PKG
+stow_package(){
+    local conflicts
+    # Check for conflicts
+    if [[ "$IS_PROFILE" = true ]]; then
+        cd $PKG_NAME
+        conflicts=$(stow --no $PROF 2>&1 | awk '/\* existing target is/ {print $NF}')
     else
-        # In case of a clonflict remove Local version and stow the Git version.
+        conflicts=$(stow --no $PKG 2>&1 | awk '/\* existing target is/ {print $NF}')
+    fi
+
+    # Remove the local version 
+    if [[ $conflicts ]]; then
         echo "Found the following conflicts:" 
-        for filename in ${CONFLICTS[@]}; do
+        for filename in ${conflicts[@]}; do
             echo $filename
             if [[ -f $HOME/$filename || -L $HOME/$filename ]]; then
                 echo "DELETE: $filename"
                 rm -f "$HOME/$filename"
             fi
         done
-        stow --verbose $PKG
     fi
-    cd $DIR  # Go back to root
-done
+
+    # Stow the git version
+    if [[ "$IS_PROFILE" = true ]]; then
+        echo "Stow $PROF profile: $PKG_NAME"
+        stow --verbose $PROF -t ~  # For profile
+    else
+        echo "Stow default: $PKG"
+        stow --verbose $PKG        # For default
+    fi
+}
+
+stow_installer() {
+    for PKG in ${CLEANED_PKGS[@]}; do
+        # Install using a user profile
+        if [[ $PKG == */$PROF ]]; then
+            IS_PROFILE=true
+            PKG_NAME=${PKG%/*}
+            stow_package
+        # Install using a defalt package
+        else
+            IS_PROFILE=false
+            stow_package
+        fi
+        cd $DIR
+    done
+}
+
+
+# Install according to a profile
+package_cleaner $PROF "${PROFILE_PKGS[@]}" "${DEFAULT_PKGS[@]}"
+stow_installer
